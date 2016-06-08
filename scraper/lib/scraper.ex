08f6e178b83ee368f4bbb801bcd82f9d19b597f3
Juan Edi <jedi@dc.uba.ar>
2016-06-08 10:13:38 -0300

    Obtención de títulos, sin supervisión

--- scraper/lib/scraper.ex
+++ scraper/lib/scraper.ex
@@ -1,2 +1,40 @@
 defmodule Scraper do
+
+  # ------- Client API
+
+  def start do
+    spawn __MODULE__, :loop, []
+  end
+
+  # ------- Private
+
+  def loop do
+    receive do
+      {from, url} ->
+        send from, {self, fetch_titles(url)}
+
+      _ ->
+        IO.puts "Message not understood :("
+    end
+    loop
+  end
+
+  defp fetch_titles(url) do
+    url
+    |> HTTPoison.get!
+    |> parse_headers
+  end
+
+  defp parse_headers(%HTTPoison.Response{status_code: 200, body: body}) do
+    Enum.flat_map ["h1", "h2", "h3"], fn selector ->
+      body
+      |> Floki.find(selector)
+      |> Enum.map(&element_text/1)
+    end
+  end
+
+  defp element_text({_tag, _attributes, [text]}) do
+    text
+  end
+
 end
